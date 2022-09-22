14091983a338d2d5b68d56a574c5699782d8858b
Jay Miller <kjaymiller@gmail.com>
2022-09-22 14:39:18 -0400

    add mypy (#6)

--- src/storage.py
+++ src/storage.py
@@ -5,9 +5,10 @@ from azure.storage.queue import QueueClient
 conn_string = os.environ.get("AZURE_STORAGE_CONNECTION_STRING")
 
 
-def load_queue(name: str) -> QueueClient:
+def load_queue(connection_string: str, name: str, **kwargs) -> QueueClient:
     """
     source: https://learn.microsoft.com/en-us/azure/storage/queues/storage-python-how-to-use-queue-storage?tabs=python%2Cenvironment-variable-windows
+    **kwargs are passed to the QueueClient connection and should be valid kwargs for the QueueClient
     """
     # Create a unique name for the queue
-    return QueueClient.from_connection_string(conn_string, name)
+    return QueueClient.from_connection_string(connection_string, name, **kwargs)
